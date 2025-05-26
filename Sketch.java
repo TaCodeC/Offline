02b00dafdbf61010cbc2800a818495775a011c36
Fernando Cortés <163168261+TaCodeC@users.noreply.github.com>
2025-05-25 18:33:26 -0600

    Buildings class base

--- Sketch.java
+++ Sketch.java
@@ -1,24 +1,49 @@
 import processing.core.PApplet;
+import processing.core.PGraphics;
+import processing.core.PVector;
+class Buildings {
+    PVector pos, size;
+    PApplet parent;
+
+    public Buildings(PApplet p) {
+        pos = new PVector(p.width/2, p.width/2, -300);
+        size = new PVector(p.random(p.width/80, p.width/40), p.random(10, 40), p.random(p.width/80, p.width/40));
+    }
+
+    public void display(PGraphics pg) {
+        pg.pushMatrix();
+        pg.translate(pos.x, pos.y, pos.z);
+        pg.fill(150, 150, 150);
+        pg.box(size.x, size.y, size.z);
+        pg.popMatrix();
+        update();
+    }
+    public void update(){
+        pos.z+=1;
+    }
+}
+
 
 public class Sketch extends PApplet {
 
-    public void settings() {
-        size(800, 600, P3D);
-    }
+    Buildings b ;
 
-    public void setup() {
-        background(30);
-    }
+        public void settings() {
+            fullScreen(P3D);
+        }
 
-    public void draw() {
-        lights();
-        background(50);
-        translate(width / 2, height / 2);
-        rotateY(frameCount * 0.01f);
-        box(100);
-    }
+        public void setup() {
+            background(90);
+            b = new Buildings(this);
+        }
 
-    public static void main(String[] args) {
-        PApplet.main("Sketch");
-    }
+        public void draw() {
+            background(0);
+            lights();
+            b.display(g);
+        }
+
+        public static void main(String[] args) {
+            PApplet.main("Sketch");
+        }
 }
